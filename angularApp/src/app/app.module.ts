b3fefaec788f5e2eecf55875d108551b35a5f9f5
Samuel Benning <s.benning66@gmail.com>
2017-06-25 16:19:44 +0200

    master: First stage complete

--- angularApp/src/app/app.module.ts
+++ angularApp/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SecondComponent } from './second/second.component';
+import { ThirdComponent } from './third.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SecondComponent,
+    ThirdComponent
   ],
   imports: [
     BrowserModule
